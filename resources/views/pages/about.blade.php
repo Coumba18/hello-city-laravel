2c9accc4a2288a18f785225cfe4a10f67ff9c893
Coumba Ngom <kumbugaan@gmail.com>
2022-06-15 15:13:01 +0000

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>About Us | Hello City</title>
+@section('content')
+     <p>Built with &hearts; by Coumba NGOM.</p>
 
-    </head>
-    <body>
-        <p>Built with &hearts; by Coumba NGOM.</p>
-        <p><a href="/">Come back to home page</a></p>
+    <p><a href="/">Come back to home page</a></p>
 
-        <footer>
-            <p>&copy; Copyright {{ date('Y')}} </p>
-        </footer>
-    </body>
-</html>
+@endsection
+    
