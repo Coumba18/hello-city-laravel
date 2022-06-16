d1167d16d83ea69916357c7b8da4ecb8c6c49e85
Coumba Ngom <kumbugaan@gmail.com>
2022-06-16 13:26:19 +0000

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,8 @@
 @section('title', 'About Us | '. config('app.name'))
 
 @section('content')
+     
+     <img src="/images/km-logo.jfif" alt="Kumba">
      <p>Built with &hearts; by Coumba NGOM.</p>
 
     <p><a href="{{route('home')}}">Come back to home page</a></p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Hello City')
 
 @section('content')
+    <img src="/images/senegal.png" alt="Senegal Flag">
     <h1>Hello from Senegal</h1>
 
     <p>It's currently {{ date('h:i A')}}.</p>
